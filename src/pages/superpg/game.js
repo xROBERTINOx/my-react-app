690776bb57324ebe0be909a2661efb1fa1050978
Papi Churro <63375372+xROBERTINOx@users.noreply.github.com>
2022-01-02 19:23:11 -0800

    doesn't work!! updated inv and working on it

--- src/pages/superpg/game.js
+++ src/pages/superpg/game.js
@@ -1,4 +1,3 @@
-import { map } from 'mathjs';
 import React,{useState} from 'react';
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 //add stats page
@@ -16,34 +15,60 @@ const Game = () => {
         return window.localStorage.getItem(item);
     }
 
-    const [invCount, setInvCount] = useState([3,1,3])
-    const [invItem, setInvItem] = useState(["swords", "coins", "other inv item"])
+    const getItemArr = (item, defaultForItem) => {
+        const check = window.localStorage.getItem(item);
+        if (!check){
+            window.localStorage.setItem(item, defaultForItem);
+        }
+        return JSON.parse(window.localStorage.getItem(item));  
+    }
+//    const gotArr = getItemArr("invItem", "['swords', 'coins', 'other inv item']");
+//    const [invItem, setInvItem] = useState(getItemArr("invCount", "[3,1,3]"));
+    const [invCount, setInvCount] = useState(getItemArr("invCount", "[3,1,3]"));
+ //   const testBefore = getItem(getItemArr("test", "[{item:'coins',amount:'3'}{item:'apples',amount:2}]"));
+//    const testAfter = JSON.parse(testBefore);
+//    const [test, setTest] = useState(getItemArr("test", "[{item:'coins',amount:'3'}{item:'apples',amount:2}]"));
 //    setInv(JSON.parse(inv))
 //    const inv = JSON.stringify(value);
 //    save `valueAsString`
 //    const parsedValue = JSON.parse(valueAsString);
 //    to change back to value  
-
-
-
     
-    const increaseInvCount = () => {
-        setInvCount([...invCount, invCount.length]);
+    const changeInvCount = (newArr) => {
+        const realNewArr = JSON.stringify(newArr);
+        window.localStorage.setItem("invCount", realNewArr);
+        setInvCount(realNewArr);
+        return;
     }
 
-    const decreaseInvCount = (index) => {
-        setInvCount(
-            invCount.filter((value, idx) => idx !== index)
-        );
+    const increaseInvCountByOne = (index) => {
+        let newArr = [...invCount]
+        newArr[index] = invCount[index] + 1
+        changeInvCount(newArr);
+    }
+
+    const decreaseInvCountByOne = (index) => {
+        let newArr = [...invCount]
+        if (newArr[index] <= 0 ) {
+            return;
+        } else if (newArr[index] >= 1) {
+            newArr[index] = invCount[index] - 1
+            changeInvCount(newArr);
+        }
     }
 
     return(
         <div>
-            {invCount.map((number, index) => (
-                
-                <h2>{number} {invItem[index]}</h2>
+            {invCount.map((number, index) => (    
+                <h2>{number}</h2>
             ))}
-            <h2>{invCount}</h2>
+            {invCount.map((number, index) => (
+                <button onClick={() => increaseInvCountByOne(index)}>Increase by one</button>
+            ))}
+            <div></div>
+            {invCount.map((number, index) => (
+                <button onClick={() => decreaseInvCountByOne(index)}>Decrease by one</button>
+            ))}            
         </div>
     );
 }
